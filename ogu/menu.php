5f97743eef2f39bc6f324b88788d5587820765d6
Gökmen Görgen <gkmngrgn@gmail.com>
2009-04-12 03:45:11 +0300

    fixed some things.

---
+++ ogu/menu.php
@@ -0,0 +1,4 @@
+<li><a href="<?php echo $url ?>/index.php?page=about">Hakkında</a></li>
+<li><a href="<?php echo $url ?>/index.php?page=facmem">Öğretim Üyeleri</a></li>
+<li><a href="<?php echo $url ?>/index.php?page=manmem">Yönetim</a></li>
+<li><a href="<?php echo $url ?>/index.php?page=contact">İletişim</a></li>
