ea801fa567aabf7ff85340fb9ba156ffdbe3807e
Gökmen Görgen <gkmngrgn@gmail.com>
2009-04-13 22:10:34 +0300

    changed page extensions from php to html.

---
+++ ogu/functions.php
@@ -0,0 +1,21 @@
+<?php
+function mainPage() {
+    include('pages/mainpage.html');
+}
+
+function administration() {
+    include('pages/administration.html');
+}
+
+function facmem() {
+    include('pages/facmem.html');
+}
+
+function lessons() {
+    include('pages/lessons.html');
+}
+
+function contact() {
+    include('pages/contact.html');
+}
+?>
